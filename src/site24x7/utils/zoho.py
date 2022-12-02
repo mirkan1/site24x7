1062161253e752f65d6d7b686f1dba393807e789
mirkan1 <mirkanbaba1@gmail.com>
2022-12-02 12:10:48 -0500

    caching, new functions for zoho outhentication

--- src/site24x7/utils/zoho.py
+++ src/site24x7/utils/zoho.py
@@ -1,6 +1,8 @@
 import json
 from datetime import datetime
 import requests
+ALL_SCOPES = "Site24x7.Account.Read, Site24x7.Account.Create, Site24x7.Account.Update, Site24x7.Account.Delete, Site24x7.Account.All, Site24x7.Reports.Read, Site24x7.Reports.Create, Site24x7.Reports.Update, Site24x7.Reports.Delete, Site24x7.Reports.All,  Site24x7.Operations.Read, Site24x7.Operations.Create, Site24x7.Operations.Update, Site24x7.Operations.Delete, Site24x7.Operations.All, Site24x7.Msp.Read, Site24x7.Msp.Create, Site24x7.Msp.Update, Site24x7.Msp.Delete, Site24x7.Msp.All, Site24x7.Bu.Read, Site24x7.Bu.Create, Site24x7.Bu.Update, Site24x7.Bu.Delete, Site24x7.Bu.All"
+
 READ_SOPES = "Site24x7.Account.Read, Site24x7.Reports.Read, Site24x7.Operations.Read"
 current_status_type = [
     'unit',
@@ -18,30 +20,48 @@ class ZohoConnection:
     ''' Oath2.0 authentication flow for Zoho API '''
     MONITOR_GRUPS = {}
     MONITORS = {}
-    def __init__(self, client_id, client_secret, code=None, refresh_token=None, access_token=None):
-        self.client_id = client_id
-        self.client_secret = client_secret
-        self.refresh_token = refresh_token
-        self.headers = {
-            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-            "Accept": "application/json; version=2.0"
-        }
-        self.access_token = access_token
-        self.expires_in = None
-        self.token_type = None
-        # 1 minute
-        self.expration_time = datetime.now().timestamp() + 60
-        self.api_domain = "https://www.site24x7.com"
-        self.auth_domain = 'https://accounts.zoho.com'
-        self.code = code
-        if not self.refresh_token and not self.code:
-            raise Exception('No refresh token or code provided')
-        if not self.refresh_token and self.code:
-            self.authorization_code(self.code)
-        elif not access_token:
-            self.refresh()
-        else:
-            self.headers['Authorization'] = 'Zoho-oauthtoken ' + self.access_token
+    MSP_CUSTOMERS = {}
+    DEVICE_KEY = None
+    def __init__(self, client_id=None, client_secret=None, code=None, refresh_token=None, access_token=None, cache=True):
+        if cache:
+            try:
+                self.pull_data_from_cache()
+            except FileNotFoundError:
+                cache = False
+        if not cache:
+            self.client_id = client_id
+            self.client_secret = client_secret
+            self.refresh_token = refresh_token
+            self.headers = {
+                "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+                "Accept": "application/json; version=2.0"
+            }
+            self.access_token = access_token
+            self.token_type = None
+            # 1 minute
+            self.expration_time = datetime.now().timestamp() + 60
+            self.api_domain = "https://www.site24x7.com"
+            self.auth_domain = 'https://accounts.zoho.com'
+            self.code = code
+            if not self.refresh_token and not self.code:
+                raise Exception('No refresh token or code provided')
+            if not self.refresh_token and self.code:
+                self.authorization_code(self.code)
+            elif not access_token:
+                self.refresh()
+            else:
+                self.headers['Authorization'] = 'Zoho-oauthtoken ' + self.access_token
+            try:
+                # this is still experimental
+                self.set_device_key()
+            except Exception as exp:
+                print(exp)
+
+    def pull_data_from_cache(self):
+        ''' Pull data from cache file '''
+        with open('.cache', 'r', encoding="utf-8") as cache_file:
+            data = json.loads(cache_file.read())
+            self.__dict__ = data
 
     def authorization_code(self, code):
         ''' Get access token from authorization code '''
@@ -58,9 +78,9 @@ class ZohoConnection:
             if "error" in response:
                 raise Exception(response["error"])
             self.access_token = response['access_token']
-            self.expires_in = response['expires_in']
             self.token_type = response['token_type']
             self.refresh_token = response['refresh_token']
+            self.expration_time = datetime.now().timestamp() + response['expires_in']
             self.headers['Authorization'] = 'Zoho-oauthtoken ' + self.access_token
 
     def refresh(self):
@@ -79,12 +99,18 @@ class ZohoConnection:
             if "error" in response:
                 raise Exception(response["error"])
             self.access_token = response['access_token']
-            self.expires_in = response['expires_in']
             self.token_type = response['token_type']
-            self.expration_time = datetime.now().timestamp() + self.expires_in
+            self.expration_time = datetime.now().timestamp() + response['expires_in']
             self.headers['Authorization'] = 'Zoho-oauthtoken ' + self.access_token
         else:
             raise Exception('Error refreshing token')
+        # save all data to a text file
+        self.save_data_into_file()
+
+    def save_data_into_file(self):
+        ''' Save all data into a file '''
+        with open('.cache', 'w', encoding="utf-8") as cache_file:
+            cache_file.write(json.dumps(self.__dict__, indent=4))
 
     def get(self, url, params:dict=None) -> dict:
         ''' Get request '''
@@ -96,8 +122,10 @@ class ZohoConnection:
         if response.status_code == 200:
             return json_response
         else:
-            raise Exception(f'Error getting response with status {response.status_code}: \
-                {json_response}')
+            error_code = json_response['error_code']
+            message = json_response['message']
+            raise Exception(f'Error getting response with status {response.status_code}:{error_code} \
+                {message}')
 
     def get_current_status_all(self):
         ''' Get all current from site24x7 status '''
@@ -178,6 +206,16 @@ class ZohoConnection:
                 up_monitors.append(self.MONITORS[key])
         return up_monitors
 
+    def get_msp_customers(self):
+        ''' Get all msp customers
+            *This function is still in development. '''
+        url = '/api/msp/customers'
+        response = self.get(url)
+        for i in response['data']:
+            name = i['name']
+            zaaid = i['zaaid']
+            self.MSP_CUSTOMERS[name] = zaaid
+
     def did_refresh(self):
         ''' Check if the token has been refreshed '''
         return self.access_token is not None
@@ -189,13 +227,30 @@ class ZohoConnection:
         if time > self.expration_time:
             raise Exception('Token expired')
 
+    def set_device_key(self):
+        ''' Set the device key '''
+        self.DEVICE_KEY = self.get_device_key()
+
+    def get_device_key(self):
+        ''' Get device key from site24x7 
+            *This function is still in development. '''
+        url = '/api/device_key'
+        response = self.get(url)
+        print("get_device_key", response)
+        return response['data']['device_key']
+
 if __name__ == '__main__':
     _client_id = "xxx"
     _client_secret = "xxx"
-    code ="xxx"
-    zoho = ZohoConnection(_client_id, _client_secret, code=code)
+    _code ="xxx"
+    zoho = ZohoConnection(_client_id, _client_secret, _code)
     print(zoho.headers)
     zoho.get_current_status_all()
     get = zoho.get('/api/current_status')
     for i in zoho.get_up_monitors():
         print(i['name'], i['status'])
+    # zoho.get_msp_customers()
+    # for i in zoho.MSP_CUSTOMERS:
+    #     print(i, zoho.MSP_CUSTOMERS[i])
+    # device_key = zoho.get_device_key()
+    # print(device_key)
